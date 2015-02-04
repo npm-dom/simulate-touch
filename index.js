e1b92092b55d5e127eafee713cf4f2980d2e7168
E.Azer Koçulu <azer@kodfabrik.com>
2015-02-04 08:37:59 -0800

    Ignore initMouseEvent errors

--- index.js
+++ index.js
@@ -10,26 +10,17 @@ function simulate (element) {
 
 function dispatch (touchEventType) {
   return function (event) {
-    var touchEvent = document.createEvent('MouseEvents');
+    var touchEvent;
 
-    touchEvent.initMouseEvent(
-      touchEventType,
-      true,             // bubbles
-      true,             // cancelable
-      window,           // view
-      1,                // detail
-      event.screenX,    // screenX
-      event.screenY,    // screenY
-      event.clientX,    // clientX
-      event.clientY,    // clientY
-      event.pageX,    // pageX
-      event.pageY,    // pageY
-      false,            // ctrlKey
-      false,            // altKey
-      false,            // shiftKey
-      false,            // metaKey
-      0,                // button
-      null);
+    try {
+      touchEvent = newTouchEvent({
+        type: touchEventType,
+        event: event
+      });
+    } catch (err) {
+      // failed to create a mouse event just ignore
+      return;
+    }
 
     touchEvent.changedTouches = touchEvent.touches = [{
       identifier: Date.now() + Math.random(),
@@ -43,4 +34,29 @@ function dispatch (touchEventType) {
 
     event.target.dispatchEvent(touchEvent);
   };
+}
+
+function newTouchEvent (options) {
+  var touchEvent = document.createEvent('MouseEvents');
+
+  touchEvent.initMouseEvent(
+    options.type,
+    true,             // bubbles
+    true,             // cancelable
+    window,           // view
+    1,                // detail
+    options.event.screenX,    // screenX
+    options.event.screenY,    // screenY
+    options.event.clientX,    // clientX
+    options.event.clientY,    // clientY
+    options.event.pageX,    // pageX
+    options.event.pageY,    // pageY
+    false,            // ctrlKey
+    false,            // altKey
+    false,            // shiftKey
+    false,            // metaKey
+    0,                // button
+    null);
+
+  return touchEvent;
 }
